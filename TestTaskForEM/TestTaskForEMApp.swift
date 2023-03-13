d94f778784ed91a85367b8137c472222d04b8cf0
Danila <korolevdanila97@gmail.com>
2023-03-13 22:31:26 +0300

    initial commit

---
+++ TestTaskForEM/TestTaskForEMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskForEMApp.swift
+//  TestTaskForEM
+//
+//  Created by Danila on 13.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskForEMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
